916a00f86aaf737539afd14e76726f7f93e7738e
Incinerator67 <edemosmanov2699@gmail.com>
2024-03-24 23:54:59 +0300

    seventh Laba - mob development basics training

--- app/src/main/java/com/android/mad/assignments/MainActivity.java
+++ app/src/main/java/com/android/mad/assignments/MainActivity.java
@@ -1,6 +1,8 @@
 package com.android.mad.assignments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+    }
+    private View.OnClickListener moveToNextScreen(){
+        return view -> {
+            Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+            String transmittedString = "string to transmit";
+            intent.putExtra(SecondActivity.TRANSMITTED_STRING,transmittedString);
+
+            Integer transmittedInt = 12;
+            intent.putExtra(SecondActivity.TRANSMITTED_INT,transmittedInt);
+
+            Boolean transmittedBoolean = false;
+            intent.putExtra(SecondActivity.TRANSMITTED_BOOLEAN,transmittedBoolean);
+
+            startActivity(intent);
+        };
     }
 }
